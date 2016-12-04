0fccdac1aea0f2551df9138e7cda357a1ff6d53f
Jing <lyovse@gmail.com>
2016-12-04 18:15:23 -0500

    start imdb movie analysis project

---
+++ projects/imdb_movies/movies.R
@@ -0,0 +1,12 @@
+## @knitr setup
+library(tidyverse)
+library(stringr)
+
+## @knitr import
+
+movies_raw <- read_csv("../data-hub/data_analysis/imdb_movies/movie_metadata.csv")
+
+## @knitr clean
+
+movies_clean <- movies_raw %>% 
+  mutate_if(is.character, str_trim)
