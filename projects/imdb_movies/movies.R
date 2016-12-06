8e758c3133812c6df8654e25d96208539649111a
Jing <lyovse@gmail.com>
2016-12-05 22:52:34 -0500

    clean data

--- projects/imdb_movies/movies.R
+++ projects/imdb_movies/movies.R
@@ -9,4 +9,7 @@ movies_raw <- read_csv("../data-hub/data_analysis/imdb_movies/movie_metadata.csv
 ## @knitr clean
 
 movies_clean <- movies_raw %>% 
-  mutate_if(is.character, str_trim)
+  mutate_if(is.character, str_trim) %>% 
+  select(-movie_imdb_link)
+
+## @knitr summary
