04890c071f1d1ad0b8d68982d58d26f5cc86c9da
Jing <lyovse@gmail.com>
2016-12-08 00:01:08 -0500

    explore some simple relationships among variables

--- projects/imdb_movies/movies.R
+++ projects/imdb_movies/movies.R
@@ -1,6 +1,7 @@
 ## @knitr setup
 library(tidyverse)
 library(stringr)
+library(ggplot2)
 
 ## @knitr import
 
@@ -13,3 +14,38 @@ movies_clean <- movies_raw %>%
   select(-movie_imdb_link)
 
 ## @knitr summary
+
+movies_clean %>% 
+  mutate_at(vars(gross, budget), function(x) x / 1000000) %>% 
+  ggplot(aes(x = imdb_score, y = gross)) +
+  geom_point() +
+  theme_light()
+
+movies_clean %>% 
+  ggplot(aes(imdb_score)) +
+  geom_histogram() +
+  theme_light()
+
+movies_clean %>% 
+  mutate_at(vars(gross, budget), function(x) x / 1000000) %>% 
+  ggplot(aes(gross)) +
+  geom_histogram() +
+  theme_light()
+
+movies_clean %>% 
+  ggplot(aes(duration)) +
+  geom_histogram() +
+  theme_light()
+
+movies_clean %>% 
+  mutate_at(vars(gross, budget), function(x) x / 1000000) %>% 
+  ggplot(aes(x = duration, y = gross)) +
+  geom_point() +
+  theme_light()
+
+movies_clean %>% 
+  ggplot(aes(x = num_voted_users, y = imdb_score)) +
+  geom_point() +
+  theme_light()
+
+
