d36bb1f052abb7bf3fb709f08e07a90cc7feafe2
Jing <lyovse@gmail.com>
2016-12-08 23:34:34 -0500

    continue exploring

--- projects/imdb_movies/movies.R
+++ projects/imdb_movies/movies.R
@@ -49,4 +49,53 @@ movies_usa %>%
   geom_point() +
   theme_light()
 
+movies_usa %>% 
+  ggplot(aes(x = budget, y = gross)) +
+  geom_point() +
+  theme_light()
 
+movies_usa %>% 
+  ggplot(aes(x = budget, y = gross)) +
+  geom_point() +
+  theme_light()
+
+movies_usa %>% 
+  mutate(cast_total_facebook_likes = cast_total_facebook_likes / 1000) %>% 
+  ggplot(aes(x = cast_total_facebook_likes, y = gross)) +
+  geom_point() +
+  theme_light()
+
+movies_usa %>% 
+  ggplot(aes(x = facenumber_in_poster, y = gross)) +
+  geom_point() +
+  theme_light()
+
+movies_usa %>% 
+  ggplot(aes(x = facenumber_in_poster, y = imdb_score)) +
+  geom_point() +
+  theme_light()
+
+## @knitr trend
+
+# average imdb_score over the years
+movies_usa %>% 
+  group_by(title_year) %>% 
+  summarise(avg_imdb_score = mean(imdb_score)) %>% 
+  ggplot(aes(title_year, avg_imdb_score)) +
+  geom_line() +
+  theme_light()
+movies_usa %>% 
+  mutate(num_voted_users = num_voted_users / 1000) %>% 
+  group_by(title_year) %>% 
+  summarise(avg_num_of_votes = mean(num_voted_users)) %>% 
+  ggplot(aes(title_year, avg_num_of_votes)) +
+  geom_line() +
+  theme_light()
+
+# facenumber over the years
+movies_usa %>% 
+  group_by(title_year) %>% 
+  summarise(avg_facenumber = mean(facenumber_in_poster)) %>% 
+  ggplot(aes(title_year, avg_facenumber)) +
+  geom_line() +
+  theme_light()
